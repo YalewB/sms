ff782b0616a158cfb59cb7fbe28f23728c60a039
YalewB <tanatec017@gmail.com>
2024-11-09 06:45:45 -0800

    I have created admin_dashboard, teacher_dashboard, student_dashboard, parent_dashboard, and finance_dashboard

--- parent_dashboard/apps.py
+++ parent_dashboard/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TeacherConfig(AppConfig):
+class ParentDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teacher'
+    name = 'parent_dashboard'
